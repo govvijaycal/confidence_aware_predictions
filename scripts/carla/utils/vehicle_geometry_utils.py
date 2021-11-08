5f79e4fbd1aeb203709dc88e5d23542306d3e89c
Vijay Govindarajan <govvijay@berkeley.edu>
2021-11-08 12:20:34 -0800

    Fixed vehicle dimensions based on Carla bounding box values, finalized scene3.

--- scripts/carla/utils/vehicle_geometry_utils.py
+++ scripts/carla/utils/vehicle_geometry_utils.py
@@ -1,14 +1,14 @@
 def vehicle_name_to_dimensions(vehicle_name):
 	if vehicle_name   == "vehicle.audi.tt":
-		l_f    = 1.25 # meters, guesstimated for now.
-		l_r    = 1.25
-		length = 4.2
-		width  = 1.8
+		l_f    = 1.25  # meters, guesstimated for now.
+		l_r    = 1.25  # meters, guesstimated for now.
+		length = 4.181 # meters (Carla BBox)
+		width  = 1.994 # meters (Carla BBox)
 	elif vehicle_name == "vehicle.mercedes-benz.coupe":
-		l_f    = 1.4  # meters, guesstimated for now.
-		l_r    = 1.4
-		length = 4.7
-		width  = 1.8
+		l_f    = 1.4   # meters, guesstimated for now.
+		l_r    = 1.4   # meters, guesstimated for now.
+		length = 5.027   # meters (Carla BBox)
+		width  = 2.152   # meters (Carla BBox)
 	else:
 		raise NotImplementedError
 
